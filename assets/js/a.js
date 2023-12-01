2d624d179b8c1e479f8b6ff2e16254e0ab81a1fc
wahyu6070 <gwtsendangrejo6070@gmail.com>
2023-12-01 20:08:11 +0700

    p

--- assets/js/a.js
+++ assets/js/a.js
@@ -1,6 +1,6 @@
 function search() {
             var searchTerm = document.getElementById("searchTerm").value;
-            var url = "https://www.google.com/search?q=" + encodeURIComponent(searchTerm);
+            var url = "https://www.google.com/search?q=litegapps+" + encodeURIComponent(searchTerm);
             window.location.href = url;
         }
 	
