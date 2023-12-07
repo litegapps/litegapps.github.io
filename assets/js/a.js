418b3587982eb6ae4c777cefc79d5102481a6853
wahyu6070 <gwtsendangrejo6070@gmail.com>
2023-12-07 16:28:54 +0700

    p

--- assets/js/a.js
+++ assets/js/a.js
@@ -1,6 +1,6 @@
 function search() {
             var searchTerm = document.getElementById("searchTerm").value;
-            var url = "https://www.google.com/search?q=litegapps+" + encodeURIComponent(searchTerm);
+            var url = "https://www.google.com/search?q=site:litegapps.github.io+" + encodeURIComponent(searchTerm);
             window.location.href = url;
         }
         
